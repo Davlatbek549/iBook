ef362f49f13a9872687cb6f0e0fb72ce8953f49c
Zafar <zaf78281@gmail.com>
2026-03-23 17:11:37 +0100

    I created packages and moved main and mock screens into those packages

--- app/src/main/java/com/example/ibook/MainActivity.kt
+++ app/src/main/java/com/example/ibook/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.ibook.screens.MainScreen
+import com.example.ibook.screens.main.MainScreen
 import com.example.ibook.ui.theme.IBookTheme
 
 class MainActivity : ComponentActivity() {
